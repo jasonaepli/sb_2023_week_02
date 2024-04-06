fce51f9eb3e57ea20219821132e08fb91d158fd2
Jason <jcaepli@gmail.com>
2024-04-05 15:23:47 -1000

    Completed FizzBuzz Exercises

--- fizzbuzz/fizzbuzz_print.cc
+++ fizzbuzz/fizzbuzz_print.cc
@@ -2,18 +2,28 @@
 
 using namespace std;
 
-////////////////////////////////
-// ADD FIZZBUZZ FUNCTION HERE //
-////////////////////////////////
+void fizzbuzz(int n){
+  // If the value of 'n' is divisible by 3 and 5
+  if(n % 3 == 0 && n % 5 == 0){
+    cout << "fizzbuzz" << endl;
+
+    // Else if the value of 'n' is divisible by 3
+  }else  if(n % 3 == 0){
+    cout << "fizz" << endl;
+
+    // Else if the value of 'n' is divisible by 5
+  }else if(n % 5 == 0){
+    cout << "buzz" << endl;
+  }else{
+    cout << n << endl;
+  }
+}
 
 int main ()
 {
   for (int n=1; n<=50; ++n)
   {
-    /////////////////////////////////////////////
-    // ADD CODE TO CALL FIZZBUZZ FUNCTION HERE //
-    /////////////////////////////////////////////
-
+    fizzbuzz(n);
   }
 }
 
--- fizzbuzz/fizzbuzz_return.cc
+++ fizzbuzz/fizzbuzz_return.cc
@@ -2,18 +2,27 @@
 
 using namespace std;
 
-////////////////////////////////
-// ADD FIZZBUZZ FUNCTION HERE //
-////////////////////////////////
+string fizzbuzz(int n){
+  if(n % 3 == 0 && n % 5 == 0){
+    return "fizzbuzz";
+  }else if(n % 3 == 0){
+    return "fizz";
+  }else if(n % 5 == 0){
+    return "buzz";
+  }else{
+    return to_string(n);
+  }
+
+}
+
 
 int main ()
 {
   for (int n=1; n<=50; ++n)
   {
-    ////////////////////////////////////////
-    // ADD CODE TO CALL FIZZBUZZ FUNCTION //
-    // AND PRINT THE RETURNED VALUE HERE  //
-    ////////////////////////////////////////
+    string result = fizzbuzz(n);
+
+    cout << result << endl;
 
   }
 }
